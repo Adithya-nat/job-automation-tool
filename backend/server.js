48425d313ac6a4e72cb6fe7031641a349a540528
adithya-nat <adithyanat26@gmail.com>
2024-12-26 15:26:05 -0800

    fix cors error

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://job-automation-tool.vercel.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://job-automation-tool-2atkv7fjt-adithyanats-projects.vercel.app"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
