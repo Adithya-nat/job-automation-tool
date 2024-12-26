360f0f8adaebf429602f276c623e6e036522a296
adithya-nat <adithyanat26@gmail.com>
2024-12-26 14:55:32 -0800

    updated env variable

--- job-helper-frontend/src/services/api.js
+++ job-helper-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.VITE_BACKEND_URL;
+const API_BASE_URL = process.env.BACKEND_ENDPOINT;
 
 const postRequest = async (url, payload) => {
   try {
