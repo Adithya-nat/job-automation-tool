01c1d47caacc724632d0434934706609311d0194
adithya-nat <adithyanat26@gmail.com>
2024-12-26 20:36:43 -0800

    updated more styles

--- job-helper-frontend/src/components/JobCard.jsx
+++ job-helper-frontend/src/components/JobCard.jsx
@@ -4,7 +4,7 @@ const JobCard = ({ job, onClick }) => {
 
   return (
     <div
-      className="p-4 bg-white rounded-lg shadow hover:shadow-lg cursor-pointer animate-slideIn"
+      className="p-4 bg-white rounded-lg shadow hover:shadow-lg cursor-pointer animate-slideIn transition ease-in-out delay-5 hover:-translate-y-1 hover:scale-110 duration-300"
       onClick={() => onClick(job)}
     >
       <h3 className="text-lg font-semibold">{title}</h3>
@@ -14,7 +14,7 @@ const JobCard = ({ job, onClick }) => {
         href={link}
         target="_blank"
         rel="noreferrer"
-        className="text-blue-500 hover:underline mt-2 inline-block"
+        className="text-indigo-600 hover:underline mt-2 inline-block"
       >
         View Job
       </a>
